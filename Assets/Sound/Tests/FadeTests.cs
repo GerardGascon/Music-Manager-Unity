2c21aa94cb135d1bf76a812003892614558103a1
Gerard Gascón <52170489+GerardGascon@users.noreply.github.com>
2024-06-23 15:04:00 +0200

    test: stop channel fade last song

--- Assets/Sound/Tests/FadeTests.cs
+++ Assets/Sound/Tests/FadeTests.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Sound.Domain.Channels;
 using Sound.Domain.Manager;
 using Sound.Tests.Mocks;
 
@@ -7,7 +8,7 @@ namespace Sound.Tests {
 		[Test]
 		public void StopAll_FadesLastChannel() {
 			MusicChannelMock mock1 = new("potato");
-			MusicChannelMock mock2 = new("potato");
+			MusicChannelMock mock2 = new("tomato");
 			MusicManager sut = new();
 
 			sut.AddChannel(mock1);
@@ -20,7 +21,7 @@ namespace Sound.Tests {
 		[Test]
 		public void StopAll_DontFadeNonLastChannel() {
 			MusicChannelMock mock1 = new("potato");
-			MusicChannelMock mock2 = new("potato");
+			MusicChannelMock mock2 = new("tomato");
 			MusicManager sut = new();
 
 			sut.AddChannel(mock1);
@@ -28,6 +29,20 @@ namespace Sound.Tests {
 			sut.StopAllChannels(1);
 
 			Assert.AreEqual(0, mock1.FadeDurationReceived);
+		}
+
+		[Test]
+		public void StopChannel_FadesLastSong() {
+			AudioTrackMock mock1 = new("potato");
+			AudioTrackMock mock2 = new("tomato");
+			MusicChannel sut = new(mock1);
+			MusicManager manager = new();
+
+			manager.AddChannel(sut);
+			sut.PlayNew(mock2);
+			manager.StopChannel(sut, 1);
+
+			Assert.AreEqual(1, mock2.FadeDurationReceived);
 		}
 	}
 }
